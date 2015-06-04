785e746fbbfe5f10baeff02f27cbd8d0e4a4b28b
xavier wu <wuzw3@mail2.sysu.edu.cn>
2015-06-04 16:46:53 +0800

    Move the enum ErrorCode to a separate file, since it may be used repeatedly, actually, it should be the same as that in the repository LinkQueue.

---
+++ LinkedStack/ErrorCode.h
@@ -0,0 +1,9 @@
+#ifndef ERRORCODE_H
+#define ERRORCODE_H
+
+enum ErrorCode
+{
+	SUCCESS, UNDERFLOW, OVERFLOW
+};
+
+#endif
--- LinkedStack/LinkedStack.h
+++ LinkedStack/LinkedStack.h
@@ -2,11 +2,7 @@
 #define LINKEDSTACK_H
 
 #include "Node.h"
-
-enum ErrorCode
-{
-	SUCCESS, UNDERFLOW, OVERFLOW
-};
+#include "ErrorCode.h"
 
 template<class StackEntry>
 class LinkedStack
